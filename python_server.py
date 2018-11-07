34ffe1b00ce7b13274d0eb24e792e8f0de2718e5
pragmatic rancho <29522373+Nirbhay191@users.noreply.github.com>
2018-11-07 17:23:19 +0530

    Update python_server.py

--- python_server.py
+++ python_server.py
@@ -2,12 +2,12 @@ import http.server
 import socketserver
 
 
-# Python Program to Get IP Address 
+# Python Program to Get IP Address of the machine server is running on 
 import socket    
 hostname = socket.gethostname()    
-IPAddr = socket.gethostbyname(hostname)    
+ipaddress = socket.gethostbyname(hostname)    
 print("Your Computer Name is:" + hostname)    
-print("Your Computer IP Address is:" + IPAddr)    
+print("Your Computer IP Address is:" + ipaddress)    
 print("to connect type ipaddress:8000 in web browser")
 
 
